a98aa13d8c8cc76ac94207acbc8ac498c11fdc84
bmb190 <bmb190@txstate.edu>
2020-04-27 00:07:18 -0500

    time

--- controller.cpp
+++ controller.cpp
@@ -71,6 +71,8 @@ void controller::work() {
                 std::cout<<"congratulations\n";
                 ui.printNode(children[i]);
                 gettimeofday(&end, NULL);
+                    const double time = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec) / 1000000.0;
+                    std::cout<<"time: "<<time <<"\n";
                     addData(start, end, children[i]);
                 }
             }
